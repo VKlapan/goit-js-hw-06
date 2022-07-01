2b43d4b1bc3229d5317de7bae77317e7c0160a00
VKlapan <vladimir.klapan@gmail.com>
2022-07-01 20:36:17 +0300

    adding task#09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColorEl = document.querySelector(".change-color");
+const colorValueEl = document.querySelector(".color");
+
+const backgroundColorChanger = () => {
+  const bgrColor = getRandomHexColor();
+  colorValueEl.textContent = bgrColor;
+  document
+    .querySelector("body")
+    .setAttribute("style", `background-color: ${bgrColor}`);
+};
+
+buttonChangeColorEl.addEventListener("click", backgroundColorChanger);
