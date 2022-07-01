55eb47e2f00d9cfc483a4fb171c47888ceade890
VKlapan <vladimir.klapan@gmail.com>
2022-07-01 20:03:53 +0300

    adding task#08-draft

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+const submitHandler = (event) => {
+  event.preventDefault();
+
+  //   const {
+  //     elements: { login, password },
+  //   } = event.currentTarget;
+
+  console.log(
+    event.currentTarget.elements.email.value,
+    event.currentTarget.elements.password.value
+  );
+};
+
+form.addEventListener("submit", submitHandler);
