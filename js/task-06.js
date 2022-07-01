880bc3f1f3e4b7f648e39b350aaee711645c5015
VKlapan <vladimir.klapan@gmail.com>
2022-07-01 16:56:12 +0300

    adding task#06

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const validationInputEl = document.querySelector("#validation-input");
+
+const inputValidation = (event) => {
+  event.currentTarget.value.length >=
+  Number.parseInt(validationInputEl.getAttribute("data-length"))
+    ? (validationInputEl.className = "valid")
+    : (validationInputEl.className = "invalid");
+};
+
+validationInputEl.addEventListener("blur", inputValidation);
+
+// console.log(validationInputEl);
