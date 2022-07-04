ed5c70599f0002cffdec400f80fd54c1749c97be
VKlapan <vladimir.klapan@gmail.com>
2022-07-04 12:55:08 +0300

    fixing task#05

--- js/task-05.js
+++ js/task-05.js
@@ -2,17 +2,20 @@ const nameInputEl = document.querySelector("#name-input");
 const nameOutputEl = document.querySelector("#name-output");
 
 const handlerInputName = (event) => {
-  nameOutputEl.textContent = event.currentTarget.value;
+  console.log(event.currentTarget.value);
+  event.currentTarget.value === ""
+    ? (nameOutputEl.textContent = "Anonymus")
+    : (nameOutputEl.textContent = event.currentTarget.value);
 };
 
-const handlerChangeName = (event) => {
-  if (event.currentTarget.value === "") {
-    nameOutputEl.textContent = "Anonymus";
-  }
-};
+// const handlerChangeName = (event) => {
+//   if (event.currentTarget.value === "") {
+//     nameOutputEl.textContent = "Anonymus";
+//   }
+// };
 
 nameInputEl.addEventListener("input", handlerInputName);
-nameInputEl.addEventListener("change", handlerChangeName);
+//nameInputEl.addEventListener("change", handlerChangeName);
 
 // console.log(nameInputEl);
 // console.log(nameOutputEl.textContent);
